124da39e8fee05484db4720d9f7ba7f42e050c8f
John Doe <johndoe@example.com>
2024-03-19 10:19:34 +0200

    add files

---
+++ MyFirstApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿//This is a coomment to my code
+
+//rakendus küsib kasutajal sisestada oma nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//string - sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" +", " + userName +"!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName} !");
+
+
+
